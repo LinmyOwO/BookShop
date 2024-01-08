55f1e0ec5fc182063c2ad3f59a93472fe87c1eb9
Linmy <grisha_shev2002@mail.ru>
2024-01-08 20:24:43 +0700

    Добавлены методы для следующих страниц: корзина, история заказов, профиль, регистрация/вход, книга, категория

--- main.py
+++ main.py
@@ -5,7 +5,48 @@ app = Flask(_name__)
 
 @app.route('/')
 def index():
+    """Главная страница"""
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    """Страница 'О нас'"""
+    return render_template("about.html")
+
+@app.route('/history')
+def history():
+    """Страница с историей заказов"""
+    return render_template("history.html", orders=[])
+
+@app.route('/cart')
+def cart():
+    """Страница корзины"""
+    return render_template("cart.html", items=[])
+
+@app.route('/register')
+def register():
+    """Страница регистрации"""
+    return render_template("register.html")
+
+@app.route('/login')
+def login():
+    """Страница входа"""
+    return render_template("login.html")
+
+@app.route('/profile/<int:userid>')
+def profile():
+    """Страница профиля"""
+    return render_template("profile.html", info={})
+
+@app.route('/book/<int:id>')
+def book():
+    """Страница книги"""
+    return render_template("book.html", bookInfo={})
+
+@app.route('/catalog/<category>')
+def catalog():
+    """Страница каталога (пока что категории)"""
+    return render_template("catalog.html")
 
 
 if __name__ == "__main__":
