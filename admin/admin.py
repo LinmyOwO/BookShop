cd781990443f6f77aed2e4918ddacd97e910ac32
Linmy <grisha_shev2002@mail.ru>
2024-01-15 02:01:36 +0700

    Начата работа с WTForms и SQLAlchemy. Точка для возврата к началу работу с WTForms

--- admin/admin.py
+++ admin/admin.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, url_for, redirect, flash, session, g
+from forms import LoginForm
 
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
@@ -16,26 +17,6 @@ def logout_admin():
     session.pop('admin_logged', None)
 
 
-@admin.route('/')
-def index():
-    return render_template('admin/index.html')
-
-
-@admin.route('/login', methods=["POST", "GET"])
-def login():
-    if isLogged():
-        return redirect(url_for('.index'))
-
-    if request.method == "POST":
-        if request.form['user'] == "admin" and request.form['psw'] == "12345":
-            login_admin()
-            return redirect(url_for('.index'))
-        else:
-            flash("Неверный логин/пароль", "error")
-
-    return render_template('admin/login.html')
-
-
 db = None
 @admin.before_request
 def before_request():
@@ -49,6 +30,37 @@ def teardown_request(request):
     global db
     db = None
     return request
+
+
+@admin.route('/')
+def index():
+    return render_template('admin/index.html')
+
+
+# @admin.route('/books')
+# def books():
+#     if not isLogged():
+#         return redirect(url_for('.login'))
+#
+#     return render_template('admin/books.html')
+
+
+@admin.route('/login', methods=["POST", "GET"])
+def login():
+    if isLogged():
+        return redirect(url_for('.index'))
+
+    form = LoginForm()
+    return render_template('admin/login.html', form=form)
+
+    # if request.method == "POST":
+    #     if request.form['user'] == "admin" and request.form['psw'] == "12345":
+    #         login_admin()
+    #         return redirect(url_for('.index'))
+    #     else:
+    #         flash("Неверный логин/пароль", "error")
+
+    # return render_template('admin/login.html')
 
 
 @admin.route('/logout', methods=["POST", "GET"])
---
+++ admin/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+
+class LoginForm(FlaskForm):
+    login = StringField("Логин", validators=[DataRequired()])
+    psw = PasswordField("Пароль", validators=[DataRequired()])
+    submit = SubmitField("Войти")
