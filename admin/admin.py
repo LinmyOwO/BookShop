e44161efa0fe4d362cac30eb7aa9a9ec572992d1
Linmy <grisha_shev2002@mail.ru>
2024-01-15 16:27:20 +0700

    Начинаю перелопачивать бд

--- admin/admin.py
+++ admin/admin.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, url_for, redirect, session, g, request, flash
-from forms import LoginForm
+from forms import LoginForm, BookForm
 
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
@@ -39,12 +39,14 @@ def index():
     return render_template('admin/index.html')
 
 
-# @admin.route('/books')
-# def books():
-#     if not isLogged():
-#         return redirect(url_for('.login'))
-#
-#     return render_template('admin/books.html')
+@admin.route('/newbook')
+def new_book():
+    if not isLogged():
+        return redirect(url_for('.login'))
+
+    form = BookForm()
+
+    return render_template('admin/newbook.html', form=form)
 
 
 @admin.route('/login', methods=["POST", "GET"])
@@ -61,15 +63,6 @@ def login():
             flash("Неверный логин/пароль")
 
     return render_template('admin/login.html', form=form)
-
-    # if request.method == "POST":
-    #     if request.form['user'] == "admin" and request.form['psw'] == "12345":
-    #         login_admin()
-    #         return redirect(url_for('.index'))
-    #     else:
-    #         flash("Неверный логин/пароль", "error")
-
-    # return render_template('admin/login.html')
 
 
 @admin.route('/logout', methods=["POST", "GET"])
--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, TextAreaField, SelectField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField, RadioField, SubmitField, IntegerField, FileField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, NumberRange, Regexp
 
 
 class LoginForm(FlaskForm):
@@ -10,5 +10,28 @@ class LoginForm(FlaskForm):
 
 
 class BookForm(FlaskForm):
-    name = TextAreaField("Название", validators=[DataRequired(), Length(max=300, message="Слишком длинное название")])
-
+    name = StringField(
+        "Название",
+        validators=[DataRequired(), Length(max=300, message="Название должно содержать меньше 300 символов")])
+    authors = StringField(
+        "Автор(-ы)",
+        validators=[DataRequired(), Length(max=300, message="Автор должен содержать меньше 300 символов")])
+    description = TextAreaField(
+        "Описание",
+        validators=[DataRequired(), Length(max=1500, message="Описание превышает 1500 символов")])
+    length = IntegerField(
+        "Количество страниц",
+        validators=[DataRequired(), NumberRange(min=1, max=10000, message="Значение должно быть целым от 1 до 10000")])
+    price = IntegerField(
+        "Цена",
+        validators=[DataRequired(), NumberRange(min=0, max=999999, message="Цена должна быть положительным и целым числом")]
+    )
+    is_available = BooleanField("Продается")
+    source = FileField(
+        "Источник",
+        validators=[Regexp('^[^\/]{1,50}\.(pdf|txt|mobi|fb3)$', message="Неверный формат файла")])
+    image = FileField(
+        "Превью",
+        validators=[Regexp('^[^\/]{1,50}\.(jpg|png)$', message="Неверный формат файла")]
+    )
+    submit = SubmitField("Добавить")
