b51cd40ebe7c23330b72c438e3d5cfcf83d3e60e
Imineko <s.m.e.-10@mail.ru>
2024-01-16 20:29:19 +0700

    bruh

---
+++ UserLogin.py
@@ -0,0 +1,29 @@
+from flask_login import UserMixin
+
+
+class UserLogin(UserMixin):
+    def fromDB(self, user_id, db_table):
+        self.__user = db_table.query.get(user_id)
+        return self
+
+    def create(self, user):
+        self.__user = user
+        self.__cart = []
+        return self
+
+    def add_to_cart(self, book):
+        if book not in self.__cart:
+            self.__cart = self.__cart.append(book)
+
+    def delete_from_cart(self, book):
+        if book in self.__cart:
+            self.__cart.remove(book)
+
+    def is_book_in_cart(self, book):
+        return book in self.__cart
+
+    def get_id(self):
+        return str(self.__user.id)
+
+    def get_cart(self):
+        return self.__cart
